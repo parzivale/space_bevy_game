4131361b5007edef44cb0bab7c1b5ee871fd2c85
parzivale <40600864+parzivale@users.noreply.github.com>
2022-09-24 10:17:10 +0800

    tidied up residual code

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,5 +1,5 @@
 use bevy::{prelude::*};
-use bevy_mod_wanderlust::*;
+
 
 
 mod player_controller;
@@ -7,10 +7,6 @@ pub struct Player;
 
 impl Plugin for Player {
     fn build(&self, app: &mut App){
-        app.add_plugin(WanderlustPlugin)
-        .insert_resource(player_controller::Sensitivity(0.15))
-        .add_startup_system(player_controller::setup_player_controller)
-        .add_system_to_stage(CoreStage::PreUpdate, player_controller::movement_input)
-        .add_system(player_controller::mouse_look);
+        app.add_plugin(player_controller::PlayerController);
     }
 }
